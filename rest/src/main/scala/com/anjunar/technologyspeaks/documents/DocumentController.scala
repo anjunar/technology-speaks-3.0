ab0a07e1919d1e44d5420992cfaad0863e718ad2
Patrick Bittner <anjunar@gmx.de>
2026-04-04 20:08:21 +0200

    bug fixing

--- rest/src/main/scala/com/anjunar/technologyspeaks/documents/DocumentController.scala
+++ rest/src/main/scala/com/anjunar/technologyspeaks/documents/DocumentController.scala
@@ -174,8 +174,7 @@ class DocumentController(val identityHolder: IdentityHolder, val documentImportS
     managed.title = entity.title
     managed.editor = entity.editor
 
-    val document = managed.merge()
-    val form = new Data(document, SchemaHateoas.enhance(document, Document.schema))
+    val form = new Data(managed, SchemaHateoas.enhance(managed, Document.schema))
 
     managed.addLinks(
       LinkBuilder.create[DocumentController](_.update(null))
